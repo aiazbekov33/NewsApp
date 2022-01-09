ccd07f5dcb2978f45b9e9b9dca4aa0bbcf07d0c1
aiazbekov33 <89203588+aiazbekov33@users.noreply.github.com>
2022-01-09 16:53:42 +0600

    Первый коммит с частичной версткой: создан Bottom_navigation ,2 recycle horizontal-vertical,  item'ы загружены все последние библиотеки на всякие случаи... созданы фрагменты

--- app/src/main/java/com/geektech/newsapp/presentation/ui/activity/MainActivity.kt
+++ app/src/main/java/com/geektech/newsapp/presentation/ui/activity/MainActivity.kt
@@ -1,9 +1,12 @@
-package com.geektech.newsapp
+package com.geektech.newsapp.presentation.ui.activity
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.geektech.newsapp.R
 
 class MainActivity : AppCompatActivity() {
+
+
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
